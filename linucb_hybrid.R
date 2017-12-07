c346ae5c0f0091cfc78004e6758e9f28478d6ac1
xuedong <shang.xuedong@yahoo.fr>
2017-12-07 19:13:44 +0100

    Re-writing hybrid model.

--- linucb_hybrid.R
+++ linucb_hybrid.R
@@ -1,26 +1,5 @@
 library(MASS)
 
-get_theta_hat_alpha <- function(b_rec, A_rec, B_rec, beta_hat){
-  return(ginv(A_rec) %*% (b_rec - B_rec %*% beta_hat))
-}
-
-get_s_t_rec <- function(z_t_rec, A_0, B_rec, A_rec, x_t_rec){
-  A_0_inv <- ginv(A_0)
-  A_rec_inv <- ginv(A_rec)
-  s_t_rec <- (t(z_t_rec) %*% A_0_inv %*% z_t_rec) - 
-    2*(t(z_t_rec) %*% A_0_inv %*% t(B_rec) %*% A_rec_inv %*% x_t_rec) +
-    (t(x_t_rec) %*% A_rec_inv %*% x_t_rec) +
-    (t(x_t_rec) %*% A_rec_inv %*% B_rec %*% A_0_inv %*% t(B_rec) %*% A_rec_inv %*% x_t_rec)
-  return(s_t_rec)
-}
-
-get_p_t_rec <- function(z_t_rec, beta_hat, x_t_rec, theta_hat_alpha, alpha, s_t_rec){
-  p_t_rec <- (t(z_t_rec) %*% beta_hat) +
-    (t(x_t_rec) %*% theta_hat_alpha) +
-    alpha * sqrt(s_t_rec)
-  return(p_t_rec)
-}
-
 update_A_0 <- function(A_0, B_rec, A_rec){
   A_rec_inv <- ginv(A_rec)
   New_A_0 <- A_0 + (t(B_rec) %*% A_rec_inv %*% B_rec)
